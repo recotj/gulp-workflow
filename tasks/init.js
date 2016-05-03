76b3f7fd957564ca160a245f9323c4528acbaa52
reco <reco@liao.com>
2016-05-03 14:14:36 +0800

    fix bug of incorrect dest path

--- tasks/init.js
+++ tasks/init.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const pkgJSON = require('../package.json');
+const where = pkgJSON['_where'];
+
 let initialized = false;
 
 module.exports = (config) => {
@@ -10,14 +13,14 @@ module.exports = (config) => {
 };
 
 function initWorkFlowConfig(config) {
+	const Path = require('path');
+
+	config = config || {};
+
 	global.PACKAGES_PATH = config.PACKAGES_PATH || 'packages';
 	global.DIST_PATH = config.DIST_PATH || 'lib';
 	global.ENTRY_FILE = config.ENTRY_FILE || 'entry.js';
 	global.PACKAGE_PREFIX = config.PACKAGE_PREFIX || '';
-
-	const execSync = require('child_process').execSync;
-	const out = execSync('npm root', { cwd: process.cwd() });
-
-	global.PACKAGE_NAME = String(out).replace(/[\r\n\s]+$/, '');
+	global.PACKAGE_NAME = require(Path.resolve(where, 'package.json')).name;
 	global.PROJECT_NAME = global.PACKAGE_NAME;
 }
